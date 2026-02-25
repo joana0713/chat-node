5ff484e6118bc330d71f61627b7bf4de41090578
joana0713 <joana0713kelly@outlook.com>
2026-02-25 04:21:48 +0400

    room-func

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Serve static files (HTML/CSS/JS)
 app.use(express.static('public'));
 
-// Store users online
-let users = {};
+let users = {}; // socket.id → { username, room }
 
-// When someone connects
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
-  
-  // User joins with username
-  socket.on('join', (username) => {
-    users[socket.id] = username;
-    io.emit('userList', Object.values(users)); // Send updated user list
-    socket.broadcast.emit('chat message', `${username} joined the chat!`);
+
+  socket.on('joinRoom', ({ username, room }) => {
+    socket.join(room);
+
+    users[socket.id] = { username, room };
+
+    // 방에만 join 메시지
+    socket.to(room).emit('chat message', `${username} joined ${room}`);
+
+    // 방에 있는 유저 리스트 보내기
+    const roomUsers = Object.values(users)
+      .filter(user => user.room === room)
+      .map(user => user.username);
+
+    io.to(room).emit('userList', roomUsers);
   });
 
-  // Handle chat messages
   socket.on('chat message', (msg) => {
-    io.emit('chat message', `${users[socket.id]}: ${msg}`);
+    const user = users[socket.id];
+    if (user) {
+      io.to(user.room).emit('chat message', `${user.username}: ${msg}`);
+    }
   });
 
-  // User disconnects
   socket.on('disconnect', () => {
-    if (users[socket.id]) {
-      io.emit('chat message', `${users[socket.id]} left the chat!`);
+    const user = users[socket.id];
+    if (user) {
+      io.to(user.room).emit('chat message', `${user.username} left the room`);
+
       delete users[socket.id];
-      io.emit('userList', Object.values(users));
+
+      const roomUsers = Object.values(users)
+        .filter(u => u.room === user.room)
+        .map(u => u.username);
+
+      io.to(user.room).emit('userList', roomUsers);
     }
   });
 });
 
-// # Add these routes to server.js (5 lines!):
-
-// if (req.url === '/') {
-//   res.end('<h1>Home Page</h1>');
-// } else if (req.url === '/about') {
-//   res.end('<h1>About Me - Full Stack Dev</h1>');
-// } else {
-//   res.end('<h1>404 - Page Not Found</h1>');
-// }
-
-server.listen(5000, () => {
-  console.log('🚀 Chat server running on http://localhost:5000');
+server.listen(5001, () => {
+  console.log('🚀 Server running on http://localhost:5001');
 });
