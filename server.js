2a341368ea52ee57676a54ab4b7c12e8364f54bf
Joana Kelly <joanak_dev@outlook.com>
2026-02-26 01:36:09 +0400

    modify

--- server.js
+++ server.js
@@ -8,44 +8,56 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let rooms = {};
+let rooms = {
+  general: [],
+  dev: [],
+  random: []
+};
 
 io.on("connection", (socket) => {
 
+  socket.emit("roomList", Object.keys(rooms));
+
   socket.on("joinRoom", (room) => {
     socket.join(room);
+    socket.currentRoom = room;
+
     if (!rooms[room]) rooms[room] = [];
+
     socket.emit("previousMessages", rooms[room]);
   });
 
-  socket.on("chatMessage", ({ room, message }) => {
+  socket.on("chatMessage", ({ text, image }) => {
+    const room = socket.currentRoom;
+    if (!room) return;
+
     const msgObj = {
       id: Date.now().toString(),
-      text: message
+      text: text || "",
+      image: image || null
     };
 
     rooms[room].push(msgObj);
     io.to(room).emit("message", msgObj);
   });
 
-  socket.on("editMessage", ({ room, id, newText }) => {
-    const msg = rooms[room].find(m => m.id === id);
-    if (msg) {
-      msg.text = newText;
-      io.to(room).emit("messageEdited", msg);
-    }
-  });
-
-  // 🔥 DELETE
-  socket.on("deleteMessage", ({ room, id }) => {
-    if (!rooms[room]) return;
+  socket.on("deleteMessage", (id) => {
+    const room = socket.currentRoom;
+    if (!room) return;
 
     rooms[room] = rooms[room].filter(m => m.id !== id);
     io.to(room).emit("messageDeleted", id);
   });
 
+  socket.on("createRoom", (roomName) => {
+    if (!rooms[roomName]) {
+      rooms[roomName] = [];
+      io.emit("roomList", Object.keys(rooms));
+    }
+  });
+
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on 3000");
 });
